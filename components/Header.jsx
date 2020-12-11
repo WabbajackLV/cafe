ace0d6a4c95067bddc8769761b168af1f6a13b4b
Arvis <55511252+WabbajackLV@users.noreply.github.com>
2020-12-11 07:20:34 +0200

    changed render type

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router";
 
-function Header() {
+export const Header = (props) => {
   return (
     <div className="topnav">
-      <Link to={"./App.jsx"}>Home</Link>
-      <Link to={"./Menu.jsx"}>Menu</Link>
-      <Link to={"./AboutUs.jsx"}>About Us</Link>
+      <Link to={"/App.jsx"}>Home</Link>
+      <Link to={"/Menu.jsx"}>Menu</Link>
+      <Link to={"/AboutUs.jsx"}>About Us</Link>
       <Link to={""}>Login</Link>
     </div>
   );
-}
-
-export default Header;
+};
