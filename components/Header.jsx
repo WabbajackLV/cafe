926019bc0866748208ead0c59c37425a04118b81
Arvis <55511252+WabbajackLV@users.noreply.github.com>
2020-12-11 08:38:50 +0200

    LAST COMIT

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { Link } from "react-router";
+// import React from "react";
+// import { Link } from "react-router";
 
-export const Header = (props) => {
+// export const Header = (props) => {
+//   return (
+//     <div className="topnav">
+//       <Link to={"/App.jsx"}>Home</Link>
+//       <Link to={"/Menu.jsx"}>Menu</Link>
+//       <Link to={"/AboutUs.jsx"}>About Us</Link>
+//       <Link to={""}>Login</Link>
+//     </div>
+//   );
+// };
+import React from "react";
+
+function Header() {
   return (
     <div className="topnav">
-      <Link to={"/App.jsx"}>Home</Link>
-      <Link to={"/Menu.jsx"}>Menu</Link>
-      <Link to={"/AboutUs.jsx"}>About Us</Link>
-      <Link to={""}>Login</Link>
+      <a>Home</a>
+      <a>Menu</a>
+      <a>About Us</a>
+      <a>Login</a>
     </div>
   );
-};
+}
+
+export default Header;
